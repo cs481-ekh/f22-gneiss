5f1bd951a2aa7a958ef9cd2daaa808b7a68649f7
Jackson Theel <62031868+jacksontheel@users.noreply.github.com>
2022-09-18 17:12:29 -0600

    Closes #14 (#45)

---
+++ back-end/src/main/kotlin/Gneiss/PacketCompiler/Service/Login.kt
@@ -0,0 +1,12 @@
+package Gneiss.PacketCompiler.Service
+
+class LoginRequest()
+class LoginResponse()
+
+class Login {
+    companion object {
+        fun login(req: LoginRequest):  LoginResponse{
+            return LoginResponse()
+        }
+    }
+}
