72d46d0ee6ffd6bc39269f246c616d74841f5d89
Connor Lawrence <connorlawrence@u.boisestate.edu>
2022-09-19 20:16:17 -0600

    Updated app file for newAccountPage

--- front-end/src/pages/newAccountPage.tsx
+++ front-end/src/pages/newAccountPage.tsx
@@ -2,3 +2,12 @@ import * as React from "react";
 import logo from "../images/logo.png";
 import image from "../images/bsu.jpg";
 
+export interface NewAccountPageProps {}
+
+export function NewAccountPage(props: NewAccountPageProps) {
+  return (
+    <div>
+      <p>Test</p>
+    </div>
+  );
+}
