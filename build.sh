348681c4816ddeebcefae1a2c4b01be6a845b087
Phillip Newell <70281330+Phillipnewell@users.noreply.github.com>
2022-09-21 18:57:54 -0600

    Dockerfiles front and back

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-service mysql start
-mysql -e "SOURCE ./init.sql"
-
+docker build --file=back-end/Dockerfile -t gneiss-back .
+docker build --file=front-end/Dockerfile -t gneiss-front .
+docker-compose -f docker-compose.yaml up
 exit 127
