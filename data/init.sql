2216edd918a19c0af3fd69cc11704613f70fe67c
humzzahargan <70771990+humzzahargan@users.noreply.github.com>
2022-09-26 23:27:17 -0600

    Closes #66 (#69)
    * Closes #66
    
    * Closes #66
    
    * Added changes suggested

--- data/init.sql
+++ data/init.sql
@@ -4,6 +4,14 @@ CREATE TABLE users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    firstname VARCHAR(50) NOT NULL UNIQUE,
-    lastname VARCHAR(50) NOT NULL UNIQUE
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    role_id INT NOT NULL
+    FOREIGN KEY(role_id) REFERENCES rolesManagement(role_id)
+);
+
+CREATE TABLE rolesManagement (
+    role_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    role VARCHAR(50) NOT NULL,
+    permission_level INT NOT NULL
 );
