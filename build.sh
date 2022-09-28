ccbf8cc0dd63317de64dd778492199b5b680bd53
aguilarjg <61432575+aguilarjg@users.noreply.github.com>
2022-09-27 19:26:51 -0600

    Closes #7 (#76)
    Closes #7

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-docker build --file=back-end/Dockerfile -t gneiss-back .
-docker build --file=front-end/Dockerfile -t gneiss-front .
-docker-compose -f docker-compose.yaml up
-exit 127
+echo "front-end: npm install"
+npm --prefix ./front-end install ./front-end || ( echo "FAILED -- front-end: npm install"; exit 1; )
+echo "front-end: npm build"
+npm --prefix ./front-end run build ./front-end || ( echo "FAILED -- front-end: npm build"; exit 1; )
+
+echo "back-end: mvn package -DskipTests"
+mvn package -f back-end/pom.xml -DskipTests || ( echo "FAILED -- back-end: mvn package -DskipTests"; exit 1; )
+
+echo "docker build back-end"
+docker build --file=back-end/Dockerfile -t gneiss-back . || ( echo "FAILED -- docker build back-end"; exit 1; )
+echo "docker build front-end"
+docker build --file=front-end/Dockerfile -t gneiss-front . || ( echo "FAILED -- docker build front-end"; exit 1; )
