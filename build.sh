4f1035d331eecd2b45fc849ae81543ab15015e85
Jose Aguilar <jaguilar@cognitics.net>
2022-09-27 18:10:43 -0600

    switched ci to use sh scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+npm --prefix ./front-end install ./front-end
+npm --prefix ./front-end run build ./front-end
+
+mvn package -f back-end/pom.xml -DskipTests
+
 docker build --file=back-end/Dockerfile -t gneiss-back .
 docker build --file=front-end/Dockerfile -t gneiss-front .
 docker-compose -f docker-compose.yaml up
-exit 127
--- test.sh
+++ test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-exit 127
+CI=true npm --prefix ./front-end test -- --coverage ./front-end
+npx --prefix ./front-end prettier ./front-end/src
+
+mvn test -f back-end/pom.xml
+curl -sSLO https://github.com/pinterest/ktlint/releases/download/0.44.0/ktlint && chmod a+x ktlint && sudo mv ktlint /usr/local/bin/
+ktlint
