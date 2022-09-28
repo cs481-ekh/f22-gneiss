06dcfecb7f9bef139f72264d700240ebf06afef6
Jose Aguilar <jaguilar@cognitics.net>
2022-09-27 18:51:23 -0600

    removing docker-compose

--- build.sh
+++ build.sh
@@ -11,5 +11,3 @@ echo "docker build back-end"
 docker build --file=back-end/Dockerfile -t gneiss-back . || ( echo "FAILED -- docker build back-end"; exit 1; )
 echo "docker build front-end"
 docker build --file=front-end/Dockerfile -t gneiss-front . || ( echo "FAILED -- docker build front-end"; exit 1; )
-echo "docker-compose"
-docker-compose -f docker-compose.yaml up || ( echo "FAILED -- docker-compose"; exit 1; )
