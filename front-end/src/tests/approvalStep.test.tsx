21f7422dbb2083e2eed31730b0651eca3794af67
Jackson Theel <jackson.l.theel@gmail.com>
2022-10-02 18:40:59 -0600

    more tests

---
+++ front-end/src/tests/approvalStep.test.tsx
@@ -0,0 +1,10 @@
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
+import { ApprovalStep } from "../components/createPacket/approvalStep";
+import { SignInForm } from "../components/signInForm";
+
+test("Email and password fields must be filled", () => {
+  let completed = false
+  render(<ApprovalStep completeStep={()=>{completed = true}}/>);
+  fireEvent.click(screen.getByText("Save"));
+  expect(completed).toBeTruthy()
+});
