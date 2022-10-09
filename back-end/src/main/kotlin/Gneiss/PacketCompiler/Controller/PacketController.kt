d180aa6dc71e4e11194ccdf72ce437a9b0b83ef5
Jose Aguilar <jaguilar@cognitics.net>
2022-10-09 16:39:48 -0600

    initial commit for packet patch request

--- back-end/src/main/kotlin/Gneiss/PacketCompiler/Controller/PacketController.kt
+++ back-end/src/main/kotlin/Gneiss/PacketCompiler/Controller/PacketController.kt
@@ -7,10 +7,13 @@ import Gneiss.PacketCompiler.Service.HighlightPDFResponse
 import Gneiss.PacketCompiler.Service.Packet
 import Gneiss.PacketCompiler.Service.PacketPostRequest
 import Gneiss.PacketCompiler.Service.PacketPostResponse
+import Gneiss.PacketCompiler.Service.PacketPatchRequest
+import Gneiss.PacketCompiler.Service.PacketPatchResponse
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.multipart.MultipartFile
 import java.util.Date
 
@@ -37,5 +40,10 @@ class PacketController {
     @PostMapping("/api/packet")
     fun PacketPost(@RequestBody req: PacketPostRequest): PacketPostResponse {
         return Packet.packetPost(req)
+    }
+
+    @PatchMapping("/api/packet")
+    fun PacketPatch(@RequestBody req: PacketPatchRequest): PacketPatchResponse {
+        return Packet.packetPatch(req)
     }
 }
--- back-end/src/main/kotlin/Gneiss/PacketCompiler/Service/Packet.kt
+++ back-end/src/main/kotlin/Gneiss/PacketCompiler/Service/Packet.kt
@@ -1,13 +1,39 @@
 package Gneiss.PacketCompiler.Service
 
 class PacketPostRequest(val invoicePDFPath: String, val approvalPDFPath: String, val csvPDFPath: String, val compiledPDFPath: String)
+class PacketPatchRequest(val invoicePDFPath: String?, val approvalPDFPath: String?, val csvPDFPath: String?, val compiledPDFPath: String?)
 
 class PacketPostResponse()
+class PacketPatchResponse( 
+    
+)
 
 class Packet {
     companion object {
+
+        var setInvoicePDFPath: String? = null
+        var setApprovalPDFPath: String? = null
+        var setCsvPDFPath: String? = null
+        var setCompiledPDFPath: String? = null
+
         fun packetPost(req: PacketPostRequest): PacketPostResponse {
             return PacketPostResponse()
+        }
+
+        fun packetPatch(req: PacketPatchRequest): PacketPatchResponse {
+            if(req.invoicePDFPath != null) {
+                setInvoicePDFPath = req.invoicePDFPath
+            }
+            if(req.approvalPDFPath != null) {
+                setApprovalPDFPath = req.approvalPDFPath
+            }
+            if(req.csvPDFPath != null) {
+                setCsvPDFPath = req.csvPDFPath
+            }
+            if(req.compiledPDFPath != null) {
+                setCompiledPDFPath = req.compiledPDFPath
+            }
+            return PacketPatchResponse()
         }
     }
 }
