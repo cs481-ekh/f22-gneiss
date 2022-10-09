5edf03698318e3b9fb1df314937135943d547ee4
Jose Aguilar <jaguilar@cognitics.net>
2022-10-09 16:54:10 -0600

    more linter fixes

--- back-end/src/main/kotlin/Gneiss/PacketCompiler/Controller/PacketController.kt
+++ back-end/src/main/kotlin/Gneiss/PacketCompiler/Controller/PacketController.kt
@@ -5,15 +5,15 @@ import Gneiss.PacketCompiler.Service.HighlightPDF
 import Gneiss.PacketCompiler.Service.HighlightPDFRequest
 import Gneiss.PacketCompiler.Service.HighlightPDFResponse
 import Gneiss.PacketCompiler.Service.Packet
-import Gneiss.PacketCompiler.Service.PacketPostRequest
-import Gneiss.PacketCompiler.Service.PacketPostResponse
 import Gneiss.PacketCompiler.Service.PacketPatchRequest
 import Gneiss.PacketCompiler.Service.PacketPatchResponse
+import Gneiss.PacketCompiler.Service.PacketPostRequest
+import Gneiss.PacketCompiler.Service.PacketPostResponse
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.multipart.MultipartFile
 import java.util.Date
 
