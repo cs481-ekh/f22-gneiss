131824cd8bd141b101737a0d6d0837c82990f572
TheodoreRamey <theodoreramey@u.boisestate.edu>
2022-10-09 12:37:37 -0600

    Updated return type to ResponseEntity to give better response codes

--- back-end/src/main/kotlin/Gneiss/PacketCompiler/Controller/UserManagementController.kt
+++ back-end/src/main/kotlin/Gneiss/PacketCompiler/Controller/UserManagementController.kt
@@ -1,22 +1,23 @@
 package Gneiss.PacketCompiler.Controller
 
+import Gneiss.PacketCompiler.DatabaseAccess.UserDao
 import Gneiss.PacketCompiler.Service.CreateTestRequest
 import Gneiss.PacketCompiler.Service.CreateTestResponse
 import Gneiss.PacketCompiler.Service.CreateUserRequest
-import Gneiss.PacketCompiler.Service.CreateUserResponse
 import Gneiss.PacketCompiler.Service.Login
 import Gneiss.PacketCompiler.Service.LoginRequest
 import Gneiss.PacketCompiler.Service.LoginResponse
 import Gneiss.PacketCompiler.Service.Test
 import Gneiss.PacketCompiler.Service.Users
-import Gneiss.PacketCompiler.DatabaseAccess.UserDao
+import org.springframework.http.HttpEntity
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("/api/user")
 class UserManagementController {
     
     val userManagementDao = UserDao()
@@ -28,7 +29,7 @@ class UserManagementController {
     }
 
     @PostMapping("/create")
-    fun CreateUser(@RequestBody req: CreateUserRequest): CreateUserResponse {
+    fun CreateUser(@RequestBody req: CreateUserRequest): ResponseEntity<String> {
         return userService.createUser(req)
     }
 
--- back-end/src/main/kotlin/Gneiss/PacketCompiler/Service/Users.kt
+++ back-end/src/main/kotlin/Gneiss/PacketCompiler/Service/Users.kt
@@ -1,7 +1,9 @@
 package Gneiss.PacketCompiler.Service
 
 import Gneiss.PacketCompiler.DatabaseAccess.UserDao
-import org.springframework.http.*
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpEntity
+import org.springframework.http.ResponseEntity
 
 class CreateUserRequest(
         var email: String,
@@ -11,26 +13,23 @@ class CreateUserRequest(
         var roleId: Int,
 )
 
-class CreateUserResponse(
-        var responseString: String
-)
-
 class Users(userDao: UserDao) {
     var userDao = userDao
 
-    fun createUser(req: CreateUserRequest): CreateUserResponse {
+    fun createUser(req: CreateUserRequest): ResponseEntity<String> {
         //Start by checking if there is already a user with the email specified in the CreateAccountRequest
         //If there is create and return a HTTP error response
         //Assume there is an account, and if not set the flag to false with checkAccountExists()
-        var accountExistsFlag: Boolean = true
-        accountExistsFlag = userDao.checkAccountExists(req.email)
+        var accountExistsFlag: Boolean = userDao.checkAccountExists(req.email)
 
         if(accountExistsFlag == true) {
-            return CreateUserResponse("Account already exists for this email")
+            return ResponseEntity<String>("Account already exists for this email", HttpStatus.NOT_ACCEPTABLE)
+            //return CreateUserResponse("Account already exists for this email")
         } else {
             //Create a new account and return a good response code
             userDao.createAccount(req.email, req.password, req.firstName, req.lastName, req.roleId)
-            return CreateUserResponse("User account created successfully")
+            return ResponseEntity<String>("User account created successfully", HttpStatus.OK)
+            //return CreateUserResponse("User account created successfully")
         }
     }
 }
