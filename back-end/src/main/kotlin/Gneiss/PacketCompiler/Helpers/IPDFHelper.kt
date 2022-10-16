018ba4eab08e2fd4d98e9686424695b3bba265f7
Jackson Theel <62031868+jacksontheel@users.noreply.github.com>
2022-10-16 12:27:19 -0600

    149 invoicepdf post (#151)
    * initial work
    
    * more work
    
    * changed name to id
    
    * name to id

--- back-end/src/main/kotlin/Gneiss/PacketCompiler/Helpers/IPDFHelper.kt
+++ back-end/src/main/kotlin/Gneiss/PacketCompiler/Helpers/IPDFHelper.kt
@@ -1,6 +1,7 @@
 package Gneiss.PacketCompiler.Helpers
 
 interface IPDFHelper {
+    fun writeFile(fileName: String, bytes: ByteArray)
     fun getTextFromPDF(pdfBytes: ByteArray): String
     fun htmlToPDF(fileName: String, htmlText: String)
 }
---
+++ back-end/src/main/kotlin/Gneiss/PacketCompiler/Service/PacketRequestHandler.kt
@@ -0,0 +1,91 @@
+package Gneiss.PacketCompiler.Service
+
+import Gneiss.PacketCompiler.Helpers.IPDFHelper
+
+class PacketPostRequest(
+    val invoicePDFPath: String,
+    val approvalPDFPath: String,
+    val csvPDFPath: String,
+    val compiledPDFPath: String
+)
+
+class PacketPatchRequest(
+    val invoicePDFPath: String?,
+    val approvalPDFPath: String?,
+    val csvPDFPath: String?,
+    val compiledPDFPath: String?
+)
+
+class ApprovalPDFPostRequest(
+    val packetId: String,
+    val outputName: String,
+    val fileBytes: ByteArray,
+    val highlightWords: Array<String>
+)
+
+class InvoicePDFPostRequest(
+    val packetId: String,
+    val outputName: String,
+    val fileBytes: ByteArray
+)
+
+class PacketPostResponse()
+
+class PacketPatchResponse()
+
+class ApprovalPDFPostResponse()
+
+class InvoicePDFPostResponse()
+
+class PacketRequestHandler(pdfHelper: IPDFHelper) {
+
+    var pdfHelper = pdfHelper
+
+    fun packetPost(req: PacketPostRequest): PacketPostResponse {
+        return PacketPostResponse()
+    }
+
+    fun packetPatch(req: PacketPatchRequest): PacketPatchResponse {
+        // if (req.invoicePDFPath != null) {
+        //     setInvoicePDFPath = req.invoicePDFPath
+        // }
+        // if (req.approvalPDFPath != null) {
+        //     setApprovalPDFPath = req.approvalPDFPath
+        // }
+        // if (req.csvPDFPath != null) {
+        //     setCsvPDFPath = req.csvPDFPath
+        // }
+        // if (req.compiledPDFPath != null) {
+        //     setCompiledPDFPath = req.compiledPDFPath
+        // }
+        return PacketPatchResponse()
+    }
+
+    fun approvalPDFPost(req: ApprovalPDFPostRequest): ApprovalPDFPostResponse {
+        var pdfText = pdfHelper.getTextFromPDF(req.fileBytes)
+
+        var htmlBuilder = StringBuilder()
+        pdfText.split("\n").forEach { htmlBuilder.append(String.format("<p>%s</p>", it)) }
+        var result = htmlBuilder.toString()
+
+        req.highlightWords.forEach {
+            result =
+                result.replace(
+                    it,
+                    String.format("<span style='background-color:yellow;'>%s</span>", it),
+                    true
+                )
+        }
+
+        pdfHelper.htmlToPDF(req.outputName, result)
+
+        return ApprovalPDFPostResponse()
+        // TODO PATCH
+    }
+
+    fun invoicePDFPost(req: InvoicePDFPostRequest): InvoicePDFPostResponse {
+        pdfHelper.writeFile(req.outputName, req.fileBytes)
+        return InvoicePDFPostResponse()
+        // TODO PATCH
+    }
+}
