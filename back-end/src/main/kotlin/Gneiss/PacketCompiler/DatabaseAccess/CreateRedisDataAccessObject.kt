f720256f64a5e95e3555c35814b46b1b8cc947d5
Phillip Newell <70281330+Phillipnewell@users.noreply.github.com>
2022-10-21 12:57:16 -0600

    Update CreateRedisDataAccessObject.kt
    getter setter and correct jedis pool added to heap

--- back-end/src/main/kotlin/Gneiss/PacketCompiler/DatabaseAccess/CreateRedisDataAccessObject.kt
+++ back-end/src/main/kotlin/Gneiss/PacketCompiler/DatabaseAccess/CreateRedisDataAccessObject.kt
@@ -1,24 +1,45 @@
 package Gneiss.PacketCompiler.DatabaseAccess
 
 import java.sql.Connection
+import java.sql.SQLException
 import redis.clients.jedis.Jedis;
 import org.springframework.stereotype.Service
 import redis.clients.jedis.JedisPool
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 class redisDao {
-    @Service
-    object CacheManager {
+
         private lateinit var jedisPool: JedisPool
 
+        
         var connection: Connection? = null
 
+        
         fun getConnection() {
             try {
-                jedisPool = JedisPool("clientName", "Jedis")
+                
+                jedisPool = JedisPool("jedisCache", 6379)
             } catch (e: SQLException) {
                 e.printStackTrace()
             }
         }
 
+        fun <T> get(key: String, clazz: Class<T>): T {
+            val value: String = getJedis().get(key)
+            val mapper = ObjectMapper().registerModule(KotlinModule())
+            mapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
+    
+            // Temp addition, rectify later.
+            mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    
+            return mapper.readValue(value, clazz)
+        }
+    
+        fun set(key: String, value: Any): Unit {
+            val mapper = ObjectMapper().writeValueAsString(value)
+            getJedis().set(key, mapper)
+        }
         fun redisPost() {
 
         }
@@ -27,5 +48,5 @@ class redisDao {
 
         }
             
-    }
+
 }
