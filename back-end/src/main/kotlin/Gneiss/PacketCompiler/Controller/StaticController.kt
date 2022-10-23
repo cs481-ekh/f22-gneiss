1096bff62975aec1e1e81da265a3ee49b6fec86a
Jackson Theel <jackson.l.theel@gmail.com>
2022-10-22 20:08:28 -0600

    lint

--- back-end/src/main/kotlin/Gneiss/PacketCompiler/Controller/StaticController.kt
+++ back-end/src/main/kotlin/Gneiss/PacketCompiler/Controller/StaticController.kt
@@ -1,18 +1,18 @@
 package Gneiss.PacketCompiler.Controller
 
+import org.springframework.boot.web.servlet.error.ErrorController
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.boot.web.servlet.error.ErrorController
 
 @Controller
-class StaticController: ErrorController {
+class StaticController : ErrorController {
 
     @RequestMapping("/error")
-    fun index():String {
-        return "index.html";
+    fun index(): String {
+        return "index.html"
     }
 
-    fun getErrorPath():String {
-        return "index.html";
+    fun getErrorPath(): String {
+        return "index.html"
     }
 }
