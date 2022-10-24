20bd7892c1df02a13df94f21d4812b5e1f0e8726
aguilarjg <61432575+aguilarjg@users.noreply.github.com>
2022-10-23 21:06:58 -0600

    Closes #141 (#161)

--- back-end/src/main/kotlin/Gneiss/PacketCompiler/PacketCompilerApplication.kt
+++ back-end/src/main/kotlin/Gneiss/PacketCompiler/PacketCompilerApplication.kt
@@ -1,9 +1,10 @@
 package Gneiss.PacketCompiler
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 class PacketCompilerApplication
 
 fun main(args: Array<String>) {
