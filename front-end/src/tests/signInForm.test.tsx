4a2be224d1a3e25a47e1cb94e0d2ea187a568b22
Connor Lawrence <connorlawrence@u.boisestate.edu>
2022-10-27 13:41:45 -0600

    Revert changes, work in main page

--- front-end/src/tests/signInForm.test.tsx
+++ front-end/src/tests/signInForm.test.tsx
@@ -1,21 +1,5 @@
 import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import { SignInForm } from "../components/signInForm";
-import { rest } from "msw";
-import { setupServer, SetupServerApi } from "msw/node";
-
-let serverResponse = 0; //setting up fake server to send requests to.
-const server: SetupServerApi = setupServer(
-  rest.post("/api/user/create", async (req, res, ctx) => {
-    console.log("string " + serverResponse);
-    return res(ctx.status(serverResponse));
-  })
-);
-const makeServerBeforeTest = (response: number) => {
-  serverResponse = response;
-  server.listen();
-};
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close);
 
 test("Email and password fields must be filled", () => {
   render(<SignInForm />);
@@ -24,7 +8,6 @@ test("Email and password fields must be filled", () => {
 });
 
 test("If input validation succeeds, go to landing page", () => {
-  makeServerBeforeTest(200); //Expect accepting (200) message from JTW check
   const result = render(<SignInForm />);
   fireEvent.change(screen.getByLabelText("Email"), {
     target: { value: "email" },
@@ -35,5 +18,3 @@ test("If input validation succeeds, go to landing page", () => {
   fireEvent.click(screen.getByText("Sign in"));
   expect(() => screen.getByRole("alert")).toThrow();
 });
-
-//Expand test for sign in with valid/invalid JWT
