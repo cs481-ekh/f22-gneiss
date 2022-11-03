616a3f60da13a5799313a1adced46a470e6047e5
Jackson Theel <jackson.l.theel@gmail.com>
2022-11-03 14:33:45 -0600

    valid JWT won't redirect you to home

--- front-end/src/data/httpService.ts
+++ front-end/src/data/httpService.ts
@@ -5,26 +5,12 @@ export const getHttpService = () => {
   const cookies = new Cookies();
   const setAuthorization = (token: string) => {
     cookies.set("Authorization", token, { sameSite: "strict" });
-    axios.interceptors.request.use(
-      function (config) {
-        if (config.headers != null) {
-          config.headers.Authorization = `${token}`;
-        }
-        return config;
-      },
-      function (error) {
-        return Promise.reject(error);
-      }
-    );
   };
 
-  const currentAuth = cookies.get("Authorization");
-  if (currentAuth != null) {
-    setAuthorization(currentAuth);
-  }
-
   return {
-    axios: axios,
+    axios: axios.create({
+      headers: {'Authorization': cookies.get("Authorization")}
+    }),
     setAuth: setAuthorization,
   };
 };
