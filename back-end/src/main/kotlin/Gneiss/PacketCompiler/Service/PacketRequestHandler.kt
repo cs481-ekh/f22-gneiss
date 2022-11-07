a5e1e3fcf87dce70a0d492e3d7acf5b8cc63d8b8
Phillip Newell <70281330+Phillipnewell@users.noreply.github.com>
2022-11-07 14:45:39 -0700

    extra import removed - cvsPDFPost cannot be ambiguous

--- back-end/src/main/kotlin/Gneiss/PacketCompiler/Service/PacketRequestHandler.kt
+++ back-end/src/main/kotlin/Gneiss/PacketCompiler/Service/PacketRequestHandler.kt
@@ -106,15 +106,14 @@ class PacketRequestHandler(pdfHelper: IPDFHelper, packetDao: IPacketDao) {
         return InvoicePDFPostResponse()
     }
 
-    fun csvPDFPost(csv: String): csvPDFPostResponse {
-        //pdfHelper.writeFile(req.outputName, req.fileBytes)
-        //packetPatch(user, id, PacketPatchRequest(null, req.outputName, null, null, null))
+    fun csvPDFPost(csv: String, id: String, req: csvPDFPostRequest): csvPDFPostResponse {
+        
         // Load source file CSV for conversion
-        var converter = Converter(csv);
+           //var converter = Converter(csv);
         // Prepare conversion options for target format PDF
-        var convertOptions = FileType().fromExtension("pdf").getConvertOptions();
+           //var convertOptions = FileType().fromExtension("pdf").getConvertOptions();
         // Convert to PDF format
-        converter.convert("output.pdf", convertOptions);
+           //converter.convert("output.pdf", convertOptions);
         return csvPDFPostResponse()
     }
 }
