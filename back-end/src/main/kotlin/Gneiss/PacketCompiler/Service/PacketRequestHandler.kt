f17b391d94dbcb0ad820f29fc4780e49afa25f06
Humzza Hargan <humzzahargan@u.boisestate.edu>
2022-11-11 19:40:36 -0700

    Yaaaaaassssss

--- back-end/src/main/kotlin/Gneiss/PacketCompiler/Service/PacketRequestHandler.kt
+++ back-end/src/main/kotlin/Gneiss/PacketCompiler/Service/PacketRequestHandler.kt
@@ -3,7 +3,6 @@ package Gneiss.PacketCompiler.Service
 import Gneiss.PacketCompiler.DatabaseAccess.IPacketDao
 import Gneiss.PacketCompiler.Helpers.IJWTHelper
 import Gneiss.PacketCompiler.Helpers.IPDFHelper
-import Gneiss.PacketCompiler.Helpers.ICSVHelper
 import Gneiss.PacketCompiler.Models.Packet
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -115,7 +114,8 @@ class PacketRequestHandler(pdfHelper: IPDFHelper, packetDao: IPacketDao) {
 
     fun csvPDFPost(user: String, id: String, req: CsvPDFPostRequest): CsvPDFPostResponse {
         var pdfText = pdfHelper.getTextFromPDF(req.fileBytes)
-        pdfHelper.csvToPDF(user)
+        var urmom = ""
+        pdfHelper.csvToPDF(pdfText, urmom)
         packetPatch(user, id, PacketPatchRequest(null, req.outputName, null, null, null))
         // Load source file CSV for conversion
            //var converter = Converter(csv);
